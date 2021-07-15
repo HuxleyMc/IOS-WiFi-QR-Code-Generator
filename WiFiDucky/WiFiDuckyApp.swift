a70b20bba27c5b298a6b0c74ca362dd7fdc42a2b
HuxleyMc <huxhales@gmail.com>
2021-07-15 22:03:12 +1000

    Initial Commit

---
+++ WiFiDucky/WiFiDuckyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiFiDuckyApp.swift
+//  WiFiDucky
+//
+//  Created by Huxley McGuffin on 15/7/21.
+//
+
+import SwiftUI
+
+@main
+struct WiFiDuckyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
