ce729ed003160ea52eb276a7628f5bf554147bba
HuxleyMc <huxhales@gmail.com>
2021-07-16 13:38:35 +1000

    Refactoring GenerateView

--- WiFiDucky/Views/GenerateView.swift
+++ WiFiDucky/Views/GenerateView.swift
@@ -11,20 +11,46 @@ import CoreImage.CIFilterBuiltins
 
 struct GenerateView: View {
     var qrString: String
-    var name: String = "Qr Code"
+    var name: String = "Network"
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
-
+    
+    @State var isPresented: Bool = false
+    
+    
+    private var QRCode: UIImage {
+        generateQRCode(from: qrString)
+    }
+    
+    
     
     var body: some View {
         VStack {
-            Image(uiImage: generateQRCode(from: qrString))
+            Image(uiImage: QRCode)
                 .interpolation(.none)
                 .resizable()
                 .scaledToFit()
                 .padding([.leading, .trailing])
-                
+            
+            
+            Button(action: {
+                isPresented = true
+            }) {
+                Label("Share", systemImage: "square.and.arrow.up")
+            }
+            .font(.headline)
+            .foregroundColor(.blue)
+            .sheet(isPresented: $isPresented) {
+                // UpScale image for saving to photos or sharing
+                ShareSheetView(
+                    activityItems: [
+                        ShareActivityItemSource(shareText: "\(name) QR Code", shareImage: QRCode),
+                        QRCode.resized(toWidth: 512) ?? QRCode
+                    ]
+                )
+            }
+            
             
         }
         .navigationTitle(name)
@@ -33,13 +59,13 @@ struct GenerateView: View {
     func generateQRCode(from string: String) -> UIImage {
         let data = Data(string.utf8)
         filter.setValue(data, forKey: "inputMessage")
-
+        
         if let outputImage = filter.outputImage {
             if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
                 return UIImage(cgImage: cgimg)
             }
         }
-
+        
         return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
